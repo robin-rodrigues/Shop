5335c74308faed80102dfbd5dffe7ab7a1a718ef
Robin Rodrigues <robinrodrigues6499@gmail.com>
2019-12-28 00:43:56 +0530

    Added secured card payment using stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PDFDocument = require('pdfkit');
+const stripe = require('stripe')(process.env.SECRET_STRIPE_KEY);
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -164,10 +165,19 @@ exports.getCheckout = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  // Token is created using Checkout or Elements!
+  // Get the payment token ID submitted by the form:
+  const token = req.body.stripeToken; // Using Express
+  let totalSum = 0;
+
   req.user
     .populate('cart.items.productId')
     .execPopulate()
-    .then(user => {
+    .then(user => {  
+      user.cart.items.forEach(p => {
+        totalSum += p.quantity * p.productId.price;
+      });
+
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
@@ -181,6 +191,13 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
+      const charge = stripe.charges.create({
+        amount: totalSum * 100,
+        currency: 'usd',
+        description: 'Demo Order',
+        source: token,
+        metadata: { order_id: result._id.toString() }
+      });
       return req.user.clearCart();
     })
     .then(() => {
